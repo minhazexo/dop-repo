62410c24bdaf7739acb530d2091429a885ed74e2
minhazexo <123165179+minhazexo@users.noreply.github.com>
2025-03-17 03:27:09 +0600

    Update Header.js.removed authentication to access academic

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -24,13 +24,8 @@ function Header() {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/academic">Academic</Link></li>
-          {isAuthenticated ? (
-            <>
-              <li><Link to="/profile">Profile</Link></li>
-              <li><button onClick={handleLogout} className="logout-button">Logout</button></li>
-            </>
-          ) : (
-            <>
+         
+
               <li><Link to="/login">Login</Link></li>
               <li><Link to="/register">Register</Link></li>
             </>
