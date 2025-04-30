f000eb17b8b9b58e9b5cce34773535f0a95f5d83
minhazexo <mehrabhossain7102@gmail.com>
2025-05-01 05:32:55 +0600

    Update Academic.js

--- src/pages/Academic/Academic.js
+++ src/pages/Academic/Academic.js
@@ -9,10 +9,10 @@ const Notes = () => {
   // Notes Data (Updated Second Year Notes with PDF Links)
   const notes = {
     "First Year": [
-      { title: " NM Calculus-1", url:  "/public/1st Year Notes/NM Stat.pdf" },
-      { title: "NM Analytical And Vector", url: "/public/1st Year Notes/NM Analytical And Vector.pdf" },
-      { title: " NM Statistics", url: "/public/1st Year Notes/NM Stat.pdf" },
-      { title:"Electricity And Magnetism", url: "/public/1st Year Notes/Electricity And Magnetism.pdf" },
+      { title: " NM Calculus-1", url:  "/1st Year Notes/NM Stat.pdf" },
+      { title: "NM Analytical And Vector", url: "1st Year Notes/NM Analytical And Vector.pdf" },
+      { title: " NM Statistics", url: "/1st Year Notes/NM Stat.pdf" },
+      { title:"Electricity And Magnetism", url: "/1st Year Notes/Electricity And Magnetism.pdf" },
     ],
     "Second Year": [
       { title: "Optics (PHA 201)(Emon 20-21)", url: "/2nd Year Notes/EMON { PHA 201 (OPTICS) }_compressed.pdf" },
